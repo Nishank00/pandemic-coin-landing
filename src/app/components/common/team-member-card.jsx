8856a16938d1fe990004aeb7ab2b53932a0e2a23
nitin3450 <nitinkanhurkar2016@gmail.com>
2024-01-06 21:19:44 +0530

    fix: flip card added and know more

--- src/app/components/common/team-member-card.jsx
+++ src/app/components/common/team-member-card.jsx
@@ -18,64 +18,93 @@ const TeamCardVariants = {
         duration: 0.1,
     },
 };
+{/* <div className="flip ">
+    <div className="front flex items-center justify-center text-center  w-fit mx-auto flex-col">
+        <Image
+            className="mx-auto bg-pdc-d-gray h-[324px] rounded-[20px]"
+            width={324}
+            height={324}
+            src={azImage}
+            quality={100}
+            alt="Pandemic Coin Logo"
+            data-aos="fade-in"
+        />
+        <div data-aos="fade-in" className="my-4">
+            <h1 className="font_cat text-[20px] text-[#7B7E98] not-italic font-normal leading-[normal] tracking-[0.8px]">Founder</h1>
+            <p className="text-[23px]">AZ</p>
+        </div>
+    </div>
+    <div className="back">
+        <div className="h-full flex flex-col justify-center items-center">
+            <div className="text-center">
+                <div className="text-[40px]">AZ</div>
+                <div className="text-[25px]">Founder</div>
+            </div>
+            <p className="text-center mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit ullam tempore possimus ea odit nisi pariatur recusandae? Dolorem sequi, eum eveniet et animi sit debitis hic ab pariatur dolores alias!</p>
+        </div>
+    </div>
+</div> */}
 
 const TeamCard = ({ img, name, desc, role, linkedin, link }) => {
     return (
-        <motion.div
-            className="rounded-[20px] relative z-30"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            variants={TeamCardVariants}
-            whileHover={{ y: 5, scale: 0.95, duration: 0.15 }}
-            whileFocus={{ y: 5, scale: 0.95, duration: 0.15 }}
-            whileTap={{ y: 5, scale: 0.95, duration: 0.15 }}
+        <div
+            className="flip rounded-[20px] relative z-30"
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1 }}
+        // variants={TeamCardVariants}
+        // whileHover={{ y: 5, scale: 0.95, duration: 0.15 }}
+        // whileFocus={{ y: 5, scale: 0.95, duration: 0.15 }}
+        // whileTap={{ y: 5, scale: 0.95, duration: 0.15 }}
         >
-            <Image
-                src={img}
-                className="mx-auto bg-pdc-d-gray h-[324px] object-cover object-top rounded-[20px]"
-                width={324}
-                height={324}
-                alt={name}
-                objectFit="contain"
-                style={{
-                    borderRadius: "30px",
-                    animation: `hover ${TeamCardVariants.hover.animationDuration} ease-in-out`,
-                    transformOrigin: "center",
-                }}
-            />
-            <div className="p-2 w-[324px] mx-auto">
-                <div className="">
-                    <motion.h1
-                        className="font_cat text-[20px] text-[#7B7E98] not-italic font-normal leading-[normal] tracking-[0.8px] mb-1"
-                        style={{ opacity: 1, transition: "opacity 0.2s ease-in-out" }}
-                        animate={{ opacity: 0.7 }}
-                        whileHover={{ opacity: 1 }}
-                    >
-                        {name}
-                    </motion.h1>
-                    <motion.p className="text-[23px]" style={{ opacity: 1 }} whileHover={{ opacity: 0.7 }}>
-                        {role}
-                    </motion.p>
-                </div>
-                {/* <motion.p className="mt-4" style={{ opacity: 1 }} whileHover={{ opacity: 0.8 }}>
-                    {desc}
-                </motion.p> */}
-                <div className="flex gap-3 mt-6">
-                    {/* <Link
-                        className="bg-pdc-blue flex w-12 h-12 rounded-full justify-center items-center"
-                        href={link}
-                    >
-                        <FaLink />
-                    </Link> */}
-                    <Link
-                        className="bg-pdc-blue flex w-12 h-12 rounded-md  justify-center items-center"
-                        href={linkedin}
-                    >
-                        <FaLinkedinIn />
-                    </Link>
+            <div className="front flex items-center justify-center text-center  w-fit mx-auto flex-col">
+                <Image
+                    src={img}
+                    className="mx-auto bg-pdc-d-gray h-[324px] object-cover object-top rounded-[20px]"
+                    width={324}
+                    height={324}
+                    alt={name}
+                    objectFit="contain"
+                    style={{
+                        borderRadius: "30px",
+                        animation: `hover ${TeamCardVariants.hover.animationDuration} ease-in-out`,
+                        transformOrigin: "center",
+                    }}
+                />
+                <div className="p-2 w-[324px] mx-auto">
+                    <div className="">
+                        <motion.h1
+                            className="font_cat text-[20px] text-[#7B7E98] not-italic font-normal leading-[normal] tracking-[0.8px] mb-1"
+                            style={{ opacity: 1, transition: "opacity 0.2s ease-in-out" }}
+                            animate={{ opacity: 0.7 }}
+                            whileHover={{ opacity: 1 }}
+                        >
+                            {name}
+                        </motion.h1>
+                        <motion.p className="text-[23px]" style={{ opacity: 1 }} whileHover={{ opacity: 0.7 }}>
+                            {role}
+                        </motion.p>
+                    </div>
+                    <div className="flex gap-3 mt-6">
+
+                        <Link
+                            className="bg-pdc-blue flex w-12 h-12 rounded-md  justify-center items-center"
+                            href={linkedin}
+                        >
+                            <FaLinkedinIn />
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </motion.div>
+            <div className="back">
+                <div className="h-full flex flex-col justify-center items-center">
+                    <div className="text-center">
+                        <div className="text-[40px]">{name}</div>
+                        <div className="text-[25px]">{role}</div>
+                    </div>
+                    <p className="text-center mt-3 max-h-[300px] p_scroll overflow-y-auto">{desc}</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
