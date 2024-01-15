bb8a631e400e5a351de3afd42e0cbf1142469fc7
dharshmakwana <harshmakwana22211@gmail.com>
2024-01-15 19:01:19 +0530

    Update grid gap in contact page layout

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
 
     return (
       <div className="bg-pdc-d-gray h-fit w-[100%] flex md:pt-[10px] pt-0 flex-col md:flex-row">
-        <div class="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-bg-pdc-d-gray text-gray-100 rounded-lg shadow-lg">
+        <div class="max-w-screen-xl mt-24 px-8 grid gap-[8rem] grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-bg-pdc-d-gray text-gray-100 rounded-lg shadow-lg">
           <div class="flex flex-col justify-between">
             <div>
               <h2 class="text-4xl lg:text-5xl font-bold leading-tight">
