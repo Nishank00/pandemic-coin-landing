9567b0794997052cd5051700aea544387d5f26e5
nitin3450 <nitinkanhurkar2016@gmail.com>
2024-01-15 22:49:32 +0530

    fix: Ai page added

---
+++ src/app/ai/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+export default function Ai() {
+    return (
+        <div className=" bg-pdc-d-gray overflow-hidden pt-0 py-[30px] md:pt-[96px] h-fit md:min-h-screen bg_ai">
+            <a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
+                <div class="card_ai">
+                    <div class="wrapper_ai">
+                        <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" class="cover-image_ai" />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" class="title_ai" />
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp" class="character_ai" />
+                </div>
+            </a>
+
+            <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
+                <div class="card_ai">
+                    <div class="wrapper_ai">
+                        <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" class="cover-image_ai" />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" class="title_ai" />
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" class="character_ai" />
+                </div>
+            </a>
+            <a href="https://www.mythrillfiction.com/the-dark-rider" alt="Mythrill" target="_blank">
+                <div class="card_ai">
+                    <div class="wrapper_ai">
+                        <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" class="cover-image_ai" />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-title.png" class="title_ai" />
+                    <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp" class="character_ai" />
+                </div>
+            </a>
+            <a href="https://www.mythrillfiction.com/force-mage" alt="Mythrill" target="_blank">
+                <div class="card_ai">
+                    <div class="wrapper_ai">
+                        <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" class="cover-image_ai" />
+                    </div>
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-title.png" class="title_ai" />
+                    <img src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp" class="character_ai" />
+                </div>
+            </a>
+        </div>
+    );
+}
